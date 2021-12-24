c3bbf77ef7b86dcfec72b4a3639be33493b66a6d
Fabian <fabian.schwab@ibm.com>
2021-12-24 14:15:23 +0100

    formik and yup validation

--- src/pages/SingIn/SignIn.js
+++ src/pages/SingIn/SignIn.js
@@ -13,72 +13,93 @@ import {
   Divider,
 } from "@mui/material";
 import { grey } from "@mui/material/colors";
+import * as yup from "yup";
+import { useFormik } from "formik";
 
 const SignIn = ({ to }) => {
-  const [credentials, setCredentials] = useState({
-    username: "",
-    password: "",
-  });
-
   const [error, setError] = useState({ show: false, msg: "" });
 
   const auth = useAuth();
   const navigate = useNavigate();
 
-  const handleSignIn = async () => {
-    const signedIn = await auth.signIn(credentials);
-    if (signedIn.auth) {
-      // Navigate to passed path
-      navigate(to);
-    } else {
-      setError({ show: true, msg: signedIn.msg });
-    }
-  };
+  const validationSchema = yup.object({
+    username: yup.string().required("Username is required"),
+    password: yup.string().required("Password is required"),
+  });
 
-  const handelInputChange = (prop) => (event) => {
-    setError({ show: false });
-    setCredentials({ ...credentials, [prop]: event.target.value });
-  };
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: async (values) => {
+      console.log(JSON.stringify(values, null, 2));
+      const signedIn = await auth.signIn(values);
+      if (signedIn.auth) {
+        // Navigate to passed path
+        navigate(to);
+      } else {
+        setError({ show: true, msg: signedIn.msg });
+      }
+    },
+  });
 
   return (
     <Container maxWidth="sm">
       <Box className="singInScreen">
-        <Stack spacing={2}>
-          <Typography variant="h1">SignIn</Typography>
-          <TextField
-            onChange={handelInputChange("username")}
-            id="username"
-            aria-label="username"
-            label="Username"
-            type="text"
-          />
-          <TextField
-            onChange={handelInputChange("password")}
-            id="password"
-            aria-label="password"
-            label="Password"
-            type="password"
-          />
-          {error.show ? (
-            <Alert variant="filled" severity="error">
-              {error.msg}
-            </Alert>
-          ) : (
-            ""
-          )}
-          <Button onClick={handleSignIn} size="large" variant="contained">
-            SignIn
-          </Button>
-          <Divider sx={{ color: grey[400] }}>or</Divider>
-          <Button
-            onClick={() => {
-              navigate("/signup");
-            }}
-            variant="outlined"
-          >
-            Sign Up
-          </Button>
-        </Stack>
+        <form onSubmit={formik.handleSubmit}>
+          <Stack spacing={2}>
+            <Typography variant="h1">SignIn</Typography>
+            <TextField
+              id="username"
+              name="username"
+              aria-label="username"
+              label="Username"
+              type="text"
+              value={formik.values.username}
+              onChange={(value) => {
+                formik.handleChange(value);
+                setError({ show: false, msg: "" });
+              }}
+              error={formik.touched.username && Boolean(formik.errors.username)}
+              helperText={formik.touched.username && formik.errors.username}
+            />
+            <TextField
+              id="password"
+              name="password"
+              aria-label="password"
+              label="Password"
+              type="password"
+              value={formik.values.password}
+              onChange={(value) => {
+                formik.handleChange(value);
+                setError({ show: false, msg: "" });
+              }}
+              error={formik.touched.password && Boolean(formik.errors.password)}
+              helperText={formik.touched.password && formik.errors.password}
+            />
+            {error.show ? (
+              <Alert variant="filled" severity="error">
+                {error.msg}
+              </Alert>
+            ) : (
+              ""
+            )}
+            <Button type="submit" size="large" variant="contained">
+              SignIn
+            </Button>
+            <Divider sx={{ color: grey[400] }}>or</Divider>
+            <Button
+              onClick={() => {
+                navigate("/signup");
+              }}
+              variant="outlined"
+            >
+              Sign Up
+            </Button>
+          </Stack>
+        </form>
       </Box>
     </Container>
   );
--- src/pages/SingUp/SignUp.js
+++ src/pages/SingUp/SignUp.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./SignUp.css";
 import {
   Container,
@@ -9,69 +9,112 @@ import {
   Button,
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import * as yup from "yup";
+import { useFormik } from "formik";
 
 const SignUp = () => {
   const navigate = useNavigate();
-  const handleSignUp = () => {
-    // validate fields
-  };
 
-  const [userInputs, setUserInputs] = useState();
+  const validationSchema = yup.object({
+    username: yup.string().required("Username is required"),
+    email: yup
+      .string()
+      .email("Enter a valid email")
+      .required("Email is required"),
+    password: yup
+      .string()
+      .min(8, "Password should be of minimum 8 characters long")
+      .required("Password is required"),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref("password"), null], "Passwords must match")
+      .required("Confirmation of the password is required"),
+  });
 
-  const handelInputChange = (prop) => (event) => {
-    if (prop === "email") {
-    }
-    setUserInputs({ ...userInputs, [prop]: event.target.value });
-  };
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
 
   return (
     <Container maxWidth="sm">
       <Box className="singUpScreen">
-        <Stack spacing={2}>
-          <Typography variant="h1">SignUp</Typography>
-          <TextField
-            onChange={handelInputChange("username")}
-            id="username"
-            aria-label="username"
-            label="Username"
-            type="text"
-          />
-          <TextField
-            onChange={handelInputChange("email")}
-            id="email"
-            aria-label="email"
-            label="E-Mail"
-            type="email"
-          />
-          <TextField
-            // onChange={handelInputChange("password")}
-            id="password"
-            aria-label="password"
-            label="Password"
-            type="password"
-          />
-          <TextField
-            // onChange={handelInputChange("confirmPassword")}
-            id="confirmPassword"
-            aria-label="confirmPassword"
-            label="Confirm Password"
-            type="password"
-          />
-          <Box className="signUpButtons">
-            <Button onClick={handleSignUp} fullWidth variant="contained">
-              SignUp
-            </Button>
-            <Button
-              fullWidth
-              onClick={() => {
-                navigate(-1);
-              }}
-              variant="outlined"
-            >
-              Cancel
-            </Button>
-          </Box>
-        </Stack>
+        <form onSubmit={formik.handleSubmit}>
+          <Stack spacing={2}>
+            <Typography variant="h1">SignUp</Typography>
+            <TextField
+              id="username"
+              name="username"
+              aria-label="username"
+              label="Username"
+              type="text"
+              value={formik.values.username}
+              onChange={formik.handleChange}
+              error={formik.touched.username && Boolean(formik.errors.username)}
+              helperText={formik.touched.username && formik.errors.username}
+            />
+            <TextField
+              id="email"
+              name="email"
+              aria-label="email"
+              label="E-Mail"
+              type="email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              error={formik.touched.email && Boolean(formik.errors.email)}
+              helperText={formik.touched.email && formik.errors.email}
+            />
+            <TextField
+              id="password"
+              name="password"
+              aria-label="password"
+              label="Password"
+              type="password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              error={formik.touched.password && Boolean(formik.errors.password)}
+              helperText={formik.touched.password && formik.errors.password}
+            />
+            <TextField
+              id="confirmPassword"
+              name="confirmPassword"
+              aria-label="confirmPassword"
+              label="Confirm Password"
+              type="password"
+              value={formik.values.confirmPassword}
+              onChange={formik.handleChange}
+              error={
+                formik.touched.confirmPassword &&
+                Boolean(formik.errors.confirmPassword)
+              }
+              helperText={
+                formik.touched.confirmPassword && formik.errors.confirmPassword
+              }
+            />
+            <Box className="signUpButtons">
+              <Button type="submit" fullWidth variant="contained">
+                SignUp
+              </Button>
+              <Button
+                fullWidth
+                onClick={() => {
+                  navigate(-1);
+                }}
+                variant="outlined"
+              >
+                Cancel
+              </Button>
+            </Box>
+          </Stack>
+        </form>
       </Box>
     </Container>
   );
