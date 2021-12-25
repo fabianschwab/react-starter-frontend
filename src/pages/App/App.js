cf3e94623f8ee953a4e3803cbeb5f3190e3e0c0c
Fabian <fabian.schwab@ibm.com>
2021-12-25 11:55:41 +0100

    deleted commet

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -90,10 +90,6 @@ function NotWhenAuthenticated({ children }) {
   const auth = useAuth();
 
   if (auth.getUser()) {
-    // Redirect them to the /login page, but save the current location they were
-    // trying to go to when they were redirected. This allows us to send them
-    // along to that page after they login, which is a nicer user experience
-    // than dropping them off on the home page.
     return <Navigate to="/profile" />;
   }
 
